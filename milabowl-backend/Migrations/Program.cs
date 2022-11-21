abd7648844bad2ef3e074cf9b25cc9db1d8d5577
Samson Svendsen <Svendsen@nordictrustee.com>
2022-11-21 14:41:50 +0100

    Upcating milabowl to .NET 7

--- milabowl-backend/Migrations/Program.cs
+++ milabowl-backend/Migrations/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,83 +12,71 @@ using Milabowl.Infrastructure.Contexts;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
-namespace Migrations
+const string CONNECTION_STRING = "Persist Security Info=False;UID=SA;Pwd=!5omeSup3rF4ncyPwd!;Database=fantasy;Server=localhost,1431; Connection Timeout=30;TrustServerCertificate=True";
+
+var configurationBuilder = new ConfigurationBuilder()
+    .AddJsonFile("./dockerappsettings.json", optional: true, reloadOnChange: true);
+
+IConfigurationRoot configuration = configurationBuilder.Build();
+Startup.Configuration = configuration;
+
+if (args[0] == "Migrate")
 {
-    class Program
+    await Migrate();
+}
+else if (args[0] == "Import")
+{
+    await Migrate();
+    await Import();
+}
+
+static async Task Migrate()
+{
+    Console.WriteLine("Running migrations...");
+    var dbContextBuilder = new DbContextOptionsBuilder<FantasyContext>(new DbContextOptions<FantasyContext>());
+    dbContextBuilder.UseSqlServer(CONNECTION_STRING);
+    await using var db = new FantasyContext(dbContextBuilder.Options);
+    await db.Database.MigrateAsync();
+    Console.WriteLine("Finished running migrations");
+}
+
+static async Task Import()
+{
+    Console.WriteLine("Importing data");
+    var serviceProvider = DI.GetServiceProvider(CONNECTION_STRING);
+    var dataImportService = serviceProvider.GetService<IDataImportService>()!;
+    var milaPointsProcessorService = serviceProvider.GetService<IMilaPointsProcessorService>()!;
+    var milaResultsService = serviceProvider.GetService<IMilaResultsService>()!;
+
+    await dataImportService.ImportData();
+    await milaPointsProcessorService.UpdateMilaPoints();
+    var milaResults = await milaResultsService.GetMilaResults();
+    var json = JsonConvert.SerializeObject(milaResults, new JsonSerializerSettings { ContractResolver = new DefaultContractResolver { NamingStrategy = new CamelCaseNamingStrategy() } });
+    await File.WriteAllTextAsync("C:\\Programming\\Other\\milabowl\\game_state.json", json);
+    Console.WriteLine("Finished importing data");
+}
+
+public static class DI
+{
+    public static IServiceProvider GetServiceProvider(string connectionString)
     {
-        static void Main(string[] args)
+        var services = new ServiceCollection();
+        services.AddScoped<IMilaResultsService, MilaResultsService>();
+        services.AddScoped<IDataImportBusiness, DataImportBusiness>();
+        services.AddScoped<IMilaPointsProcessorService, MilaPointsProcessorService>();
+        services.AddScoped<IMilaRuleBusiness, MilaRuleBusiness>();
+        services.AddScoped<IDataImportService, DataImportService>();
+        services.AddScoped<IMilaResultsBusiness, MilaResultsBusiness>();
+        services.AddScoped<IDataImportProvider, DataImportProvider>();
+        services.AddScoped<IFantasyMapper, FantasyMapper>();
+        services.AddDbContext<FantasyContext>(optionsBuilder =>
         {
-            var configurationBuilder = new ConfigurationBuilder()
-                .AddJsonFile("./dockerappsettings.json", optional: true, reloadOnChange: true);
+            optionsBuilder.UseSqlServer(connectionString, options => options.EnableRetryOnFailure());
+        });
+        services.AddHttpClient();
 
-            IConfigurationRoot configuration = configurationBuilder.Build();
-            Startup.Configuration = configuration;
-
-            if (args[0] == "Migrate")
-            {
-                Migrate();
-            }
-            else if (args[0] == "Import")
-            {
-                Migrate();
-                Import().GetAwaiter().GetResult();
-            }
-        }
-
-        static void Migrate()
-        {
-            Console.WriteLine("Running migrations...");
-            var dbContextBuilder = new DbContextOptionsBuilder<FantasyContext>(new DbContextOptions<FantasyContext>());
-            dbContextBuilder.UseSqlServer("Persist Security Info=False;UID=SA;Pwd=yourStrong(!)Password;Database=fantasy;Server=localhost,1436; Connection Timeout=30");
-            using var db = new FantasyContext(dbContextBuilder.Options);
-            db.Database.Migrate();
-            Console.WriteLine("Finished running migrations");
-        }
-
-        static async Task Import()
-        {
-            Console.WriteLine("Importing data");
-            //var builder = new WebHostBuilder()
-            //    .UseConfiguration(Startup.Configuration)
-            //    .UseContentRoot(Directory.GetCurrentDirectory())
-            //    .UseStartup<Startup>()
-            //    .Build();
-
-            //var dataImportService = (DataImportService)builder.Services.GetService(typeof(IDataImportService));
-            //var milaPointsProcessorService = (MilaPointsProcessorService)builder.Services.GetService(typeof(IMilaPointsProcessorService));
-
-            var serviceProvider = GetServiceProvider();
-            var dataImportService = serviceProvider.GetService<IDataImportService>();
-            var milaPointsProcessorService = serviceProvider.GetService<IMilaPointsProcessorService>();
-            var milaResultsService = serviceProvider.GetService<IMilaResultsService>();
-
-            await dataImportService.ImportData();
-            await milaPointsProcessorService.UpdateMilaPoints();
-            var milaResults = await milaResultsService.GetMilaResults();
-            var json = JsonConvert.SerializeObject(milaResults, new JsonSerializerSettings{ ContractResolver = new DefaultContractResolver{ NamingStrategy = new CamelCaseNamingStrategy() }  });
-            await File.WriteAllTextAsync("C:\\Programming\\Other\\milabowl\\game_state.json", json);
-            Console.WriteLine("Finished importing data");
-        }
-
-        private static IServiceProvider GetServiceProvider()
-        {
-            var services = new ServiceCollection();
-            services.AddScoped<IMilaResultsService, MilaResultsService>();
-            services.AddScoped<IDataImportBusiness, DataImportBusiness>();
-            services.AddScoped<IMilaPointsProcessorService, MilaPointsProcessorService>();
-            services.AddScoped<IMilaRuleBusiness, MilaRuleBusiness>();
-            services.AddScoped<IDataImportService, DataImportService>();
-            services.AddScoped<IMilaResultsBusiness, MilaResultsBusiness>();
-            services.AddScoped<IDataImportProvider, DataImportProvider>();
-            services.AddScoped<IFantasyMapper, FantasyMapper>();
-            services.AddDbContext<FantasyContext>(optionsBuilder =>
-            {
-                var connectionString = "Persist Security Info=False;UID=SA;Pwd=yourStrong(!)Password;Database=fantasy;Server=localhost,1436; Connection Timeout=30";
-                optionsBuilder.UseSqlServer(connectionString, options => options.EnableRetryOnFailure());
-            });
-            services.AddHttpClient();
-
-            return services.BuildServiceProvider(); ;
-        }
+        return services.BuildServiceProvider(); ;
     }
 }
+
+
