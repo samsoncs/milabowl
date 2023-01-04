324718ec9c51ebe64d3a135c73c72c744eecd15d
Samson Svendsen <Svendsen@nordictrustee.com>
2023-01-04 15:57:41 +0100

    fixed blog mobile view

--- milabowl-frontend/src/Blog/BlogPage.tsx
+++ milabowl-frontend/src/Blog/BlogPage.tsx
@@ -1,34 +1,44 @@
-import { Grid, Card, CardContent, CardHeader, Typography } from "@mui/material";
+import {
+  Grid,
+  Card,
+  CardContent,
+  CardHeader,
+  Typography,
+  Container
+} from "@mui/material";
 import React from "react";
 import { blogEntries } from "./blogEntries";
 
 const BlogPage: React.FC<{}> = () => (
-  <Grid
-    container
-    spacing={2}
+  <Container
     maxWidth="md"
+    disableGutters
     style={{ marginLeft: "auto", marginRight: "auto" }}
   >
-    {blogEntries.map((b) => (
-      <Grid item xs={12} key={b.title}>
-        <Card>
-          <CardHeader title={<Typography variant="h5">{b.title}</Typography>} />
-          <CardContent>
-            {b.paragraphs.map((p, i) => (
-              <Typography
-                style={{ marginTop: i !== 0 ? "1rem" : "" }}
-                component="p"
-                key={p}
-                variant="body1"
-              >
-                {p}
-              </Typography>
-            ))}
-          </CardContent>
-        </Card>
-      </Grid>
-    ))}
-  </Grid>
+    <Grid container spacing={2}>
+      {blogEntries.map((b) => (
+        <Grid item xs={12} key={b.title}>
+          <Card>
+            <CardHeader
+              title={<Typography variant="h5">{b.title}</Typography>}
+            />
+            <CardContent>
+              {b.paragraphs.map((p, i) => (
+                <Typography
+                  style={{ marginTop: i !== 0 ? "1rem" : "" }}
+                  component="p"
+                  key={p}
+                  variant="body1"
+                >
+                  {p}
+                </Typography>
+              ))}
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  </Container>
 );
 
 export default BlogPage;
