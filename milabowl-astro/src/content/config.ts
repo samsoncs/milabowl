29096acb505695d72e054a7917904e9c7c8b99b8
Anders Kristiansen <andersk.kristiansen@gmail.com>
2023-08-09 13:19:27 +0200

    blog beste lagnavn

--- milabowl-astro/src/content/config.ts
+++ milabowl-astro/src/content/config.ts
@@ -1,22 +1,22 @@
-import { z, defineCollection }  from "astro:content";
+import {defineCollection, z} from "astro:content";
 
-const blogCollection = defineCollection({ 
-  schema: z.object({
-    title: z.string().max(60),
-    date: z.date(),
-    author: z.enum(["MilaCorp", "Sam"]),
-    tags: z.enum(["GW Summary", "Announcement", "Tech"]).array().optional().default([])
-  })
+const blogCollection = defineCollection({
+    schema: z.object({
+        title: z.string().max(60),
+        date: z.date(),
+        author: z.enum(["MilaCorp", "Sam", "Anders", "Simen", "Henrik", "Mikkel", "Malte", "Eivind", "Markus", "Martin"]),
+        tags: z.enum(["GW Summary", "Announcement", "Tech"]).array().optional().default([])
+    })
 });
 
-const rulesCollection = defineCollection({ 
-  schema: z.object({
-    title: z.string().max(60),
-    date: z.date(),
-  })
+const rulesCollection = defineCollection({
+    schema: z.object({
+        title: z.string().max(60),
+        date: z.date(),
+    })
 });
 
 export const collections = {
-  'blog': blogCollection,
-  'rules': rulesCollection
+    'blog': blogCollection,
+    'rules': rulesCollection
 };
