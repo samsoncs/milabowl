4ae5f7d5b7ba53c3f59a637722534cc0b0f948b6
mikkelos <mikkel.treu@gmail.com>
2023-08-10 12:40:36 +0200

    hide overflow

--- milabowl-astro/src/components/core/PowerUpModal.tsx
+++ milabowl-astro/src/components/core/PowerUpModal.tsx
@@ -16,7 +16,7 @@ const PowerUpModal: React.FC<PowerUpModalProps> = ({ modal_id, name, icon, info 
 
     return (
         <div>
-            <div id={modal_id} className="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-auto overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+            <div id={modal_id} className="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
                 <div className="relative w-full max-w-md max-h-full">
                     <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
                         <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide={modal_id}>
