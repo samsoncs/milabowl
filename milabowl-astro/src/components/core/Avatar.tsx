20078b63cff08604e4d96b36fca4b960426d08c0
mikkelos <mikkel.treu@gmail.com>
2023-08-11 12:54:45 +0200

    Remove hardcoded test values

--- milabowl-astro/src/components/core/Avatar.tsx
+++ milabowl-astro/src/components/core/Avatar.tsx
@@ -118,15 +118,6 @@ const Avatar: React.FC<AvatarProps> = ({ children, teamName }) => {
         powerUpGWs[key] = gw;
     }
 
-    if (teamName.includes("Start")) {
-        powerUpGWs = {
-            "3xc": "",
-            "wildcard": "",
-            "freehit": "",
-            "bboost": "",
-        };
-    }
-
     return (
         <div className={styles.avatarDiv}>
             <div data-popover-target={"popover-user-profile" + teamName.replaceAll(" ", "")} className={styles.avatar + " w-6 md:w-7 lg:w-9"}>
