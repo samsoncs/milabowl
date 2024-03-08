63cdc99801fe71d8401c407cd91b97d5211e4570
Samson Svendsen <samson.svendsen@vipps.no>
2024-03-08 11:32:30 +0100

    Refactor

--- milabowl-backend/Milabowl.Processing/DataImport/UserGameWeek.cs
+++ milabowl-backend/Milabowl.Processing/DataImport/UserGameWeek.cs
@@ -1,5 +1,4 @@
-﻿using Milabowl.Processing.DataImport.MilaDtos;
-using Milabowl.Processing.Processing;
+﻿using Milabowl.Processing.Processing;
 
 namespace Milabowl.Processing.DataImport;
 
@@ -25,86 +24,113 @@ public record User(
     int EventTotal
 );
 
-public record UserGameWeek(
-    Event @Event,
-    HeadToHeadEvent HeadToHeadEvent,
-    User User,
-    IList<PlayerEvent> Lineup,
-    string? ActiveChip,
-    IList<UserGameWeek> HistoricUserGameWeeks,
-    IList<UserGameWeek> HistoricOpponantGameWeeks
-)
+public class UserGameWeek
 {
+    public int GameWeek => _event.GameWeek;
+    public UserGameWeek? PreviousGameWeek => _previousGameWeek;
+    public int UserId => _user.Id;
+    public IList<PlayerEvent> Lineup { get; }
+    public IList<PlayerEvent> SubsIn { get; }
+    public IList<PlayerEvent> SubsOut { get; }
+    public decimal TotalScore { get; }
+    public decimal CumulativeTotalScore { get; }
+    public decimal AvgCumulativeTotalScore { get; }
     public IList<UserGameWeek> Opponents { get; private set; } = new List<UserGameWeek>();
+    public int GwPosition { get; private set; }
+    public int MilaRank { get; private set; }
+    public int MilaRankLastWeek { get; private set; }
+    public decimal TotalCumulativeAvgMilaScore { get; private set; }
+
+    public decimal TotalMilaScore { get; private set; }
+
+    public decimal CumulativeTotalMilaScore { get; private set; }
+
+    public decimal AvgCumulativeTotalMilaScore { get; private set; }
+
+    private readonly UserGameWeek? _previousGameWeek;
+
+    // TODO: Check - do we need separate lists for user and opponents for historic?
+    private readonly IList<UserGameWeek> _historicUserGameWeeks;
+    private readonly IList<UserGameWeek> _historicOpponentGameWeeks;
+    private readonly Event _event;
+    private readonly User _user;
     private IList<MilaRuleResult> _ruleResults = new List<MilaRuleResult>();
+
+    public UserGameWeek(
+        Event @event,
+        HeadToHeadEvent headToHeadEvent,
+        User user,
+        IList<PlayerEvent> lineup,
+        string? activeChip,
+        IList<UserGameWeek> historicUserGameWeeks,
+        IList<UserGameWeek> historicOpponantGameWeeks
+    )
+    {
+        _event = @event;
+        _user = user;
+        Lineup = lineup;
+        _historicUserGameWeeks = historicUserGameWeeks;
+        _historicOpponentGameWeeks = historicOpponantGameWeeks;
+        _previousGameWeek = historicUserGameWeeks.FirstOrDefault(h =>
+            h.GameWeek == @event.GameWeek - 1
+        );
+        SubsIn = _previousGameWeek is null
+            ? []
+            : Lineup
+                .Where(pe =>
+                    _previousGameWeek.Lineup.All(ipe =>
+                        ipe.FantasyPlayerEventId != pe.FantasyPlayerEventId
+                    )
+                )
+                .ToList();
+        SubsOut = _previousGameWeek is null
+            ? []
+            : _previousGameWeek
+                .Lineup.Where(pe =>
+                    Lineup.All(ipe => ipe.FantasyPlayerEventId != pe.FantasyPlayerEventId)
+                )
+                .ToList();
+        TotalScore = Lineup.Sum(l => l.TotalPoints * l.Multiplier);
+        CumulativeTotalScore = historicUserGameWeeks.Sum(h => h.TotalScore) + TotalScore;
+        AvgCumulativeTotalScore = Math.Round(CumulativeTotalScore / GameWeek, 2);
+    }
 
     public void AddOpponentsForGameWeek(IList<UserGameWeek> userGameWeeks)
     {
-        Opponents = userGameWeeks.Where(u => u.User.Id != User.Id).ToList();
+        Opponents = userGameWeeks.Where(u => u.UserId != _user.Id).ToList();
+        GwPosition = Opponents.Sum(u => u.TotalScore > TotalScore ? 1 : 0) + 1;
+
+        AvgCumulativeTotalMilaScore = Math.Round(
+            (
+                _historicOpponentGameWeeks.Sum(h => h.TotalMilaScore)
+                + Opponents.Sum(h => h.TotalMilaScore)
+                + TotalMilaScore
+            ) / (_historicOpponentGameWeeks.Count + Opponents.Count + 1),
+            2
+        );
     }
 
     public void AddRuleResult(List<MilaRuleResult> results)
     {
         _ruleResults = results;
+        TotalMilaScore = results.Sum(r => r.Points);
+        CumulativeTotalMilaScore =
+            _historicUserGameWeeks.Sum(h => h.TotalMilaScore) + TotalMilaScore;
+        TotalCumulativeAvgMilaScore = Math.Round(CumulativeTotalMilaScore / GameWeek, 2);
+        MilaRank =
+            Opponents.Sum(o => o.CumulativeTotalMilaScore > CumulativeTotalMilaScore ? 1 : 0) + 1;
+        MilaRankLastWeek =
+            _historicOpponentGameWeeks
+                .Where(h => h.GameWeek == GameWeek - 1)
+                .Sum(o =>
+                    o.CumulativeTotalMilaScore
+                    > _historicUserGameWeeks
+                        .FirstOrDefault(h => h.GameWeek == GameWeek - 1)
+                        ?.CumulativeTotalMilaScore
+                        ? 1
+                        : 0
+                ) + 1;
     }
-
-    public UserGameWeek? PreviousGameWeek =>
-        HistoricUserGameWeeks.FirstOrDefault(h => h.Event.GameWeek == Event.GameWeek - 1);
-
-    public List<PlayerEvent> SubsIn =>
-        PreviousGameWeek is null
-            ? []
-            : Lineup
-                .Where(pe =>
-                    PreviousGameWeek.Lineup.All(ipe =>
-                        ipe.FantasyPlayerEventId != pe.FantasyPlayerEventId
-                    )
-                )
-                .ToList();
-
-    public List<PlayerEvent> SubsOut =>
-        PreviousGameWeek is null
-            ? []
-            : PreviousGameWeek
-                .Lineup.Where(pe =>
-                    Lineup.All(ipe => ipe.FantasyPlayerEventId != pe.FantasyPlayerEventId)
-                )
-                .ToList();
-
-    public decimal TotalScore => Lineup.Sum(l => l.TotalPoints * l.Multiplier);
-    public decimal CumulativeTotalScore =>
-        HistoricUserGameWeeks.Sum(h => h.TotalScore) + TotalScore;
-    public decimal AvgCumulativeTotalScore => Math.Round(CumulativeTotalScore / Event.GameWeek, 2);
-    public int GwPosition =>
-        Opponents //.Where(u => u.User.Id != User.Id)
-        .Sum(u => u.TotalScore > TotalScore ? 1 : 0) + 1;
-    public decimal TotalMilaScore => _ruleResults.Sum(r => r.Points);
-    public decimal CumulativeTotalMilaScore =>
-        HistoricUserGameWeeks.Sum(h => h.TotalMilaScore) + TotalMilaScore;
-    public decimal AvgCumulativeTotalMilaScore =>
-        Math.Round(CumulativeTotalMilaScore / Event.GameWeek, 2);
-    public decimal TotalCumulativeAverageMilaScore =>
-        Math.Round(
-            (
-                HistoricOpponantGameWeeks.Sum(h => h.TotalMilaScore)
-                + Opponents.Sum(h => h.TotalMilaScore)
-                + TotalMilaScore
-            ) / (HistoricOpponantGameWeeks.Count + Opponents.Count + 1),
-            2
-        );
-    public int MilaRank =>
-        Opponents.Sum(o => o.CumulativeTotalMilaScore > CumulativeTotalMilaScore ? 1 : 0) + 1;
-    public int MilaRankLastWeek =>
-        HistoricOpponantGameWeeks
-            .Where(h => h.Event.GameWeek == Event.GameWeek - 1)
-            .Sum(o =>
-                o.CumulativeTotalMilaScore
-                > HistoricUserGameWeeks
-                    .FirstOrDefault(h => h.Event.GameWeek == Event.GameWeek - 1)
-                    ?.CumulativeTotalMilaScore
-                    ? 1
-                    : 0
-            ) + 1;
 
     public Dictionary<string, RuleResult> Rules =>
         _ruleResults.ToDictionary(k => k.RuleName, e => new RuleResult(e.Points, e.RuleShortName));
