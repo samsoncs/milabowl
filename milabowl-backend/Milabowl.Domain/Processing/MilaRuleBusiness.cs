2a6193324257d824b97c923fcdc6553a2a766f87
Simen Aakhus <aakhus.simen@gmail.com>
2025-05-12 21:36:58 +0200

    Update MilaRuleBusiness.cs
    RedCard -> 6
    CapDef -> 0
    IncreaseStreak -> 0

--- milabowl-backend/Milabowl.Domain/Processing/MilaRuleBusiness.cs
+++ milabowl-backend/Milabowl.Domain/Processing/MilaRuleBusiness.cs
@@ -42,7 +42,7 @@ namespace Milabowl.Domain.Processing
         }
         public decimal GetCapDefScore(IList<MilaRuleData> playerEvents, Player currentUserCaptain)
         {
-            return playerEvents.Any(pe => pe.PlayerPosition == 2 && currentUserCaptain.PlayerId == pe.Player.PlayerId && pe.Minutes > 45) ? 1 : 0;
+            return 0; //playerEvents.Any(pe => pe.PlayerPosition == 2 && currentUserCaptain.PlayerId == pe.Player.PlayerId && pe.Minutes > 45) ? 1 : 0;
         }
 
         public decimal GetSixtyNine(IList<MilaRuleData> playerEvents)
@@ -62,7 +62,7 @@ namespace Milabowl.Domain.Processing
 
         public decimal GetRedCardScore(IList<MilaRuleData> playerEvents)
         {
-            return playerEvents.Sum(pe => pe.RedCards * pe.Multiplier * 3);
+            return playerEvents.Sum(pe => pe.RedCards * pe.Multiplier * 6);
         }
         public decimal GetMinusIsPlusScore(IList<MilaRuleData> playerEvents)
         {
@@ -122,7 +122,8 @@ namespace Milabowl.Domain.Processing
                     increaseStreakCount = 0;
                 }
             }
-            return increaseStreakCount > 1 ? 1 : 0;
+            return 0;
+            //return increaseStreakCount > 1 ? 1 : 0;
         }
 
         public decimal GetHeadToHeadMetaScore(UserHeadToHead userHeadToHeadDto)
