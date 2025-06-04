075a2f3f4d01fd00919e27cbefed3047582660ba
samson.svendsen@vipps.no <samson.svendsen@vipps.no>
2025-06-04 15:50:25 +0200

    adding initial milalytics

---
+++ milabowl-backend/Milabowl.Milalytics/Program.cs
@@ -0,0 +1,198 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+using Milabowl.Milalytics;
+using Milabowl.Processing;
+using Milabowl.Processing.DataImport;
+
+var serviceProvider = DependencyInjection.GetServiceProvider();
+var importer = serviceProvider.GetRequiredService<FplImporter>();
+var importedData = await importer.ImportFplDataForRulesProcessing();
+
+foreach (var gameWeekState in importedData)
+{
+    var managerGameWeek = new ManagerGameWeek(
+        Guid.NewGuid(),
+        gameWeekState.User.User.UserName,
+        gameWeekState.User.User.TeamName,
+        gameWeekState.User.User.Id,
+        gameWeekState.User.Event.GameWeek,
+        gameWeekState.User.ActiveChip
+    );
+
+    await using var connection = new SqlConnection("Persist Security Info=False;UID=SA;Pwd=!5omeSup3rF4ncyPwd!;Database=fantasy;Server=localhost,1431; Connection Timeout=30;TrustServerCertificate=True");
+    await connection.ExecuteAsync("""
+        INSERT INTO ManagerGameWeek (
+            ManagerGameWeekId,
+            ManagerName,
+            FantasyTeamName,
+            FantasyEntryId,
+            GameWeek,
+            ActiveChip
+        )
+        VALUES (
+            @ManagerGameWeekId,
+            @ManagerName,
+            @FantasyTeamName,
+            @FantasyEntryId,
+            @GameWeek,
+            @ActiveChip
+        )
+    """,
+    new
+    {
+        managerGameWeek.ManagerGameWeekId,
+        managerGameWeek.ManagerName,
+        managerGameWeek.FantasyTeamName,
+        managerGameWeek.FantasyEntryId,
+        managerGameWeek.GameWeek,
+        managerGameWeek.ActiveChip
+    });
+
+    foreach (var playerEvent in gameWeekState.User.Lineup)
+    {
+        var managerGameWeekLineup = new ManagerGameWeekLineup(
+            managerGameWeek.ManagerGameWeekId,
+            playerEvent.Multiplier,
+            playerEvent.IsCaptain,
+            playerEvent.IsViceCaptain,
+            playerEvent.FantasyPlayerEventId,
+            playerEvent.FirstName,
+            playerEvent.Surname,
+            playerEvent.WebName,
+            playerEvent.PlayerPosition,
+            1,
+            1,
+            "Liverpool",
+            "LFC",
+            playerEvent.Minutes,
+            playerEvent.GoalsScored,
+            playerEvent.Assists,
+            playerEvent.CleanSheets,
+            playerEvent.GoalsConceded,
+            playerEvent.OwnGoals,
+            playerEvent.PenaltiesSaved,
+            playerEvent.PenaltiesMissed,
+            playerEvent.YellowCards,
+            playerEvent.RedCards,
+            playerEvent.Saves,
+            playerEvent.Bonus,
+            playerEvent.Bps,
+            playerEvent.Influence,
+            playerEvent.Creativity,
+            playerEvent.Threat,
+            playerEvent.IctIndex,
+            playerEvent.TotalPoints,
+            playerEvent.InDreamteam
+        );
+
+        await using var cnn = new SqlConnection("Persist Security Info=False;UID=SA;Pwd=!5omeSup3rF4ncyPwd!;Database=fantasy;Server=localhost,1431; Connection Timeout=30;TrustServerCertificate=True");
+        await cnn.ExecuteAsync(
+            """
+                   INSERT INTO ManagerGameWeekLineup (
+                       FkManagerGameWeekId,
+                       Multiplier,
+                       IsCaptain,
+                       IsViceCaptain,
+                       FantasyPlayerEventId,
+                       FirstName,
+                       LastName,
+                       WebName,
+                       ElementType,
+                       FantasyTeamId,
+                       FantasyTeamCode,
+                       TeamName,
+                       TeamShortName,
+                       Minutes,
+                       GoalsScored,
+                       Assists,
+                       CleanSheets,
+                       GoalsConceded,
+                       OwnGoals,
+                       PenaltiesSaved,
+                       PenaltiesMissed,
+                       YellowCards,
+                       RedCards,
+                       Saves,
+                       Bonus,
+                       Bps,
+                       Influence,
+                       Creativity,
+                       Threat,
+                       IctIndex,
+                       TotalPoints,
+                       InDreamTeam
+                   )
+                   VALUES (
+                       @FkManagerGameWeekId,
+                       @Multiplier,
+                       @IsCaptain,
+                       @IsViceCaptain,
+                       @FantasyPlayerEventId,
+                       @FirstName,
+                       @LastName,
+                       @WebName,
+                       @ElementType,
+                       @FantasyTeamId,
+                       @FantasyTeamCode,
+                       @TeamName,
+                       @TeamShortName,
+                       @Minutes,
+                       @GoalsScored,
+                       @Assists,
+                       @CleanSheets,
+                       @GoalsConceded,
+                       @OwnGoals,
+                       @PenaltiesSaved,
+                       @PenaltiesMissed,
+                       @YellowCards,
+                       @RedCards,
+                       @Saves,
+                       @Bonus,
+                       @Bps,
+                       @Influence,
+                       @Creativity,
+                       @Threat,
+                       @IctIndex,
+                       @TotalPoints,
+                       @InDreamTeam
+                   )
+               """,
+            new
+            {
+                FkManagerGameWeekId = managerGameWeek.ManagerGameWeekId,
+                managerGameWeekLineup.Multiplier,
+                managerGameWeekLineup.IsCaptain,
+                managerGameWeekLineup.IsViceCaptain,
+                managerGameWeekLineup.FantasyPlayerEventId,
+                managerGameWeekLineup.FirstName,
+                managerGameWeekLineup.LastName,
+                managerGameWeekLineup.WebName,
+                managerGameWeekLineup.ElementType,
+                managerGameWeekLineup.FantasyTeamId,
+                managerGameWeekLineup.FantasyTeamCode,
+                managerGameWeekLineup.TeamName,
+                managerGameWeekLineup.TeamShortName,
+                managerGameWeekLineup.Minutes,
+                managerGameWeekLineup.GoalsScored,
+                managerGameWeekLineup.Assists,
+                managerGameWeekLineup.CleanSheets,
+                managerGameWeekLineup.GoalsConceded,
+                managerGameWeekLineup.OwnGoals,
+                managerGameWeekLineup.PenaltiesSaved,
+                managerGameWeekLineup.PenaltiesMissed,
+                managerGameWeekLineup.YellowCards,
+                managerGameWeekLineup.RedCards,
+                managerGameWeekLineup.Saves,
+                managerGameWeekLineup.Bonus,
+                managerGameWeekLineup.Bps,
+                managerGameWeekLineup.Influence,
+                managerGameWeekLineup.Creativity,
+                managerGameWeekLineup.Threat,
+                managerGameWeekLineup.IctIndex,
+                managerGameWeekLineup.TotalPoints,
+                managerGameWeekLineup.InDreamTeam
+            });
+    }
+}
+
---
+++ milabowl-backend/Milabowl.Processing/Infrastructure/denormalized.sql
@@ -0,0 +1,68 @@
+CREATE TABLE [dbo].[ManagerGameWeek](
+    [ManagerGameWeekId] [uniqueidentifier] NOT NULL DEFAULT newid(),
+    [ManagerName] [nvarchar](max) NOT NULL,
+    [FantasyTeamName] [nvarchar](max) NOT NULL,
+    [FantasyEntryId] [int] NOT NULL,
+    --[FantasyResultId] [int] NOT NULL,
+    [GameWeek] [int] NOT NULL,
+    [ActiveChip] [nvarchar](max) NULL,
+
+    CONSTRAINT [PK_ManagerGameWeek] PRIMARY KEY CLUSTERED([ManagerGameWeekId] ASC)
+)
+
+CREATE TABLE [dbo].[ManagerGameWeekLineup](
+    [ManagerGameWeekLineupId] [uniqueidentifier] NOT NULL DEFAULT newid(),
+    [FkManagerGameWeekId] [uniqueidentifier] NOT NULL,
+    [Multiplier] [int] NOT NULL,
+    [IsCaptain] [bit] NOT NULL,
+    [IsViceCaptain] [bit] NOT NULL,
+    [FantasyPlayerEventId] [int] NOT NULL,
+
+    -- Player data
+    [FirstName] [nvarchar](max) NOT NULL,
+    [LastName] [nvarchar](max) NOT NULL,
+    [WebName] [nvarchar](max) NOT NULL,
+    --[FantasyPlayerId] [int] NOT NULL,
+    --[Code] [int] NOT NULL,
+    [ElementType] [int] NOT NULL,
+    --[News] [nvarchar](max) NOT NULL,
+    --[NewsAdded] [datetime2](7) NULL,
+    --[Photo] [nvarchar](max) NOT NULL,
+    --[Status] [nvarchar](max) NOT NULL,
+
+    -- Team data
+    [FantasyTeamId] [int] NOT NULL,
+    [FantasyTeamCode] [int] NOT NULL,
+    [TeamName] [nvarchar](max) NOT NULL,
+    [TeamShortName] [nvarchar](max) NOT NULL,
+
+    -- GameWeek data
+    [Minutes] [int] NOT NULL,
+    [GoalsScored] [int] NOT NULL,
+    [Assists] [int] NOT NULL,
+    [CleanSheets] [int] NOT NULL,
+    [GoalsConceded] [int] NOT NULL,
+    [OwnGoals] [int] NOT NULL,
+    [PenaltiesSaved] [int] NOT NULL,
+    [PenaltiesMissed] [int] NOT NULL,
+    [YellowCards] [int] NOT NULL,
+    [RedCards] [int] NOT NULL,
+    [Saves] [int] NOT NULL,
+    [Bonus] [int] NOT NULL,
+    [Bps] [int] NOT NULL,
+    [Influence] [nvarchar](max) NOT NULL,
+    [Creativity] [nvarchar](max) NOT NULL,
+    [Threat] [nvarchar](max) NOT NULL,
+    [IctIndex] [nvarchar](max) NOT NULL,
+    [TotalPoints] [int] NOT NULL,
+    [InDreamteam] [bit] NOT NULL,
+    --[Selected] [int] NULL,
+    --[TransferBalance] [int] NULL,
+    --[TransfersIn] [int] NULL,
+    --[TransfersOut] [int] NULL,
+    --[Value] [decimal](18, 2) NULL,
+
+    CONSTRAINT [PK_ManagerGameWeekLineup] PRIMARY KEY CLUSTERED([ManagerGameWeekLineupId] ASC),
+    CONSTRAINT FK_ManagerGameWeek_UserGameWeekLineup FOREIGN KEY (FkManagerGameWeekId) REFERENCES ManagerGameWeek(ManagerGameWeekId)
+    )
+
