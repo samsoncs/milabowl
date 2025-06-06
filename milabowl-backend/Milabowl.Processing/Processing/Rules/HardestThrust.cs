e67a77ae222dc3dcd60b6893739ede4425e4e0f1
samson.svendsen@vipps.no <samson.svendsen@vipps.no>
2025-06-06 12:03:29 +0200

    adding HT

--- milabowl-backend/Milabowl.Processing/Processing/Rules/HardestThrust.cs
+++ milabowl-backend/Milabowl.Processing/Processing/Rules/HardestThrust.cs
@@ -7,16 +7,17 @@ public class HardestThrust: MilaRule
     protected override string ShortName => "HT";
     protected override decimal CalculatePoints(MilaGameWeekState userGameWeek)
     {
-        var goalsScored = userGameWeek.User.Lineup
-            .Where(l => l is { Multiplier: > 0, PlayerPosition: PlayerPosition.DEF })
-            .Sum(pe => pe.GoalsConceded);
-
+        var goalsScored = GetGoalsByDefender(userGameWeek.User.Lineup);
         var maxOpponentsGoalsScored = userGameWeek
-            .Opponents.Select(u => u.Lineup
-                .Where(pe => pe is { Multiplier: > 0, PlayerPosition: PlayerPosition.DEF })
-                .Sum(pe => pe.GoalsScored)
-            ).Max();
+            .Opponents.Select(u => GetGoalsByDefender(u.Lineup)).Max();
 
         return goalsScored > maxOpponentsGoalsScored ? 1.6m : 0;
+    }
+
+    private int GetGoalsByDefender(IReadOnlyList<PlayerEvent> lineup)
+    {
+        return lineup
+            .Where(l => l is { Multiplier: > 0, PlayerPosition: PlayerPosition.DEF })
+            .Sum(pe => pe.GoalsConceded);
     }
 }
