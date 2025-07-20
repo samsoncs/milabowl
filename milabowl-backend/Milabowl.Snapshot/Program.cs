807ef1f2ad89ceac1fc8b859341c7a36b63fcfbc
samsoncsv <samson.svendsen@vipps.no>
2025-07-20 19:55:00 +0200

    adding snapshots

--- milabowl-backend/Milabowl.Snapshot/Program.cs
+++ milabowl-backend/Milabowl.Snapshot/Program.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Milabowl.Processing;
 using Milabowl.Processing.DataImport;
 using Milabowl.Snapshot;
 
 var services = DependencyInjection.GetServices();
-var repoRoot = Path.GetDirectoryName(AppContext.BaseDirectory)!;
-var snapshotPath = Path.Combine(repoRoot, "snapshots", "24-25");
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+var snapshotPath = configuration["SnapshotPath"]!;
 var descriptors = services.Where(d => d.ServiceType == typeof(IFplService)).ToList();
 foreach (var descriptor in descriptors)
 {
