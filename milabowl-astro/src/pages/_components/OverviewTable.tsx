fc73ea5995ecb43a7e32caf61caf0d63822f0042
samsoncsv <samson.svendsen@vipps.no>
2025-08-04 16:04:41 +0200

    updating overview table

--- milabowl-astro/src/pages/_components/OverviewTable.tsx
+++ milabowl-astro/src/pages/_components/OverviewTable.tsx
@@ -1,27 +1,8 @@
-import {
-  createColumnHelper,
-  type RowData,
-  type ExpandedState,
-} from '@tanstack/react-table';
-import type { GameWeekResult } from '../../game_state/gameState';
-import SortableTable from '../../components/core/Table/SortableTable';
-import { useMemo, useState } from 'react';
 import PositionDelta from '../../components/core/PositionDelta';
-import '@tanstack/react-table';
-import TrendChart from './TrendChart';
-import type { ResultsForTeams } from './types';
-import TeamDetailPanel from './TeamDetailPanel';
+import type { GameWeekResult } from '../../game_state/gameState';
 import './style.css';
-
-declare module '@tanstack/react-table' {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  interface ColumnMeta<TData extends RowData, TValue> {
-    align?: 'right' | 'left';
-    classNames?: string;
-  }
-}
-
-const columnHelper = createColumnHelper<GameWeekResult>();
+import { useMemo, useState, type MouseEventHandler } from 'react';
+import TeamDetailPanel from './TeamDetailPanel';
 
 type OptimizedImage = {
   src: string;
@@ -30,190 +11,242 @@ type OptimizedImage = {
   sizes: string;
 };
 
-interface OverviewTableProps {
+interface OverviewTable2Props {
   data: GameWeekResult[];
-  teams: ResultsForTeams[];
   avatars: OptimizedImage[];
-  currentGameWeekResults?: GameWeekResult[];
+  currentGameWeekResults: GameWeekResult[] | undefined;
 }
 
-const OverviewTable: React.FC<OverviewTableProps> = ({
+interface SortToggleProps {
+  sortDirection?: 'asc' | 'desc';
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+function useSortedTable<T>(
+  data: T[],
+  initialProp: keyof T,
+  initialOrder: 'asc' | 'desc' = 'desc'
+) {
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>(initialOrder);
+  const [sortProp, setSortProp] = useState<keyof T>(initialProp);
+
+  const handleSort = (prop: keyof T) => {
+    if (sortProp === prop) {
+      setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    } else {
+      setSortProp(prop);
+      setSortOrder('desc');
+    }
+  };
+
+  const sortedData: T[] = useMemo(() => {
+    return [...data].sort((a, b) => {
+      const aVal = a[sortProp];
+      const bVal = b[sortProp];
+      if (typeof aVal === 'number' && typeof bVal === 'number') {
+        return sortOrder === 'asc' ? aVal - bVal : bVal - aVal;
+      }
+      if (typeof aVal === 'string' && typeof bVal === 'string') {
+        return sortOrder === 'asc'
+          ? aVal.localeCompare(bVal)
+          : bVal.localeCompare(aVal);
+      }
+      return 0;
+    });
+  }, [data, sortProp, sortOrder]);
+
+  return { sortedData, sortOrder, sortProp, handleSort };
+}
+
+const SortToggle = ({ sortDirection, onClick }: SortToggleProps) => (
+  <>
+    <button onClick={onClick}>
+      {sortDirection === 'asc' && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="h-5 w-5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12"
+          />
+        </svg>
+      )}
+      {sortDirection === 'desc' && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="h-5 w-5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0-3.75-3.75M17.25 21 21 17.25"
+          />
+        </svg>
+      )}
+      {!sortDirection && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="h-5 w-5 text-slate-400 dark:text-slate-500"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
+          />
+        </svg>
+      )}
+    </button>
+  </>
+);
+
+const Chevron = () => (
+  <svg
+    className={`h-4 w-4 transition-transform duration-200`}
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M19 9l-7 7-7-7"
+    />
+  </svg>
+);
+
+const OverviewTable = ({
   data,
   avatars,
-  teams,
   currentGameWeekResults,
-}) => {
-  const [expanded, setExpanded] = useState<ExpandedState>({});
-
-  const renderSubComponent = ({
-    row,
-  }: {
-    row: { original: GameWeekResult };
-  }) => (
-    <TeamDetailPanel
-      row={row.original}
-      teams={teams}
-      currentGameWeekResults={currentGameWeekResults}
-    />
-  );
-
-  const lastGameWeek = data[data.length - 1].gameWeek;
-  const columns = useMemo(
-    () => [
-      columnHelper.display({
-        id: 'milaRank',
-        header: '#',
-        cell: (props) => {
-          const deltaPosition =
-            props.row.original.milaRankLastWeek === null ||
-            props.row.original.milaRankLastWeek === undefined
-              ? 0
-              : props.row.original.milaRankLastWeek -
-                props.row.original.milaRank;
-
-          return (
-            <div className="flex flex-col items-center justify-between gap-1">
-              <PositionDelta
-                pos={props.row.original.milaRank}
-                delta={deltaPosition}
-              />
-            </div>
-          );
-        },
-      }),
-      columnHelper.accessor('teamName', {
-        id: 'teamName',
-        header: 'Team',
-        cell: (props) => {
-          const optimizedImage = avatars.find((a) =>
-            a.src.includes(
-              props.row.original.teamName
-                .replace('$', 's')
-                .toLowerCase()
-                .replaceAll(' ', '_')
-            )
-          )!;
-          return (
-            <span
-              className={`flex items-center gap-2 rounded-l-full rank-${props.row.original.milaRank}`}
-            >
-              <picture className="h-10 w-10 rounded-full sm:h-12 sm:w-12">
-                <source
-                  srcSet={optimizedImage.avif.join(', ')}
-                  sizes={optimizedImage.sizes}
-                  type="image/avif"
-                />
-                <source
-                  srcSet={optimizedImage.webp.join(', ')}
-                  sizes={optimizedImage.sizes}
-                  type="image/webp"
-                />
-                <img
-                  src={optimizedImage.src}
-                  className="rounded-full"
-                  alt={`${props.row.original.teamName} avatar`}
-                />
-              </picture>
-              <a
-                className={`max-w-[75px] truncate transition-all duration-200 hover:underline sm:max-w-[250px] ${props.row.original.milaRank < 4 ? 'font-bold' : ''}`}
-                href={`/fpl/players/${props.row.original.teamName.replaceAll(' ', '-')}/gw/${lastGameWeek}`}
-              >
-                {props.cell.getValue()}
-              </a>
-            </span>
-          );
-        },
-        enableSorting: false,
-      }),
-      columnHelper.display({
-        id: 'trend',
-        header: 'Trend',
-        cell: (props) => (
-          <>
-            <TrendChart
-              height={30}
-              teamName={props.row.original.teamName}
-              results={teams
-                .find((r) => r.teamName === props.row.original.teamName)!
-                .results.slice(-5)}
-            />
-          </>
-        ),
-        meta: {
-          classNames: 'hidden sm:table-cell lg:hidden xl:table-cell',
-        },
-      }),
-      columnHelper.accessor('gwScore', {
-        id: 'gwScore',
-        header: 'GW',
-        meta: {
-          align: 'right',
-        },
-      }),
-      columnHelper.accessor('cumulativeMilaPoints', {
-        id: 'cumulativeMilaPoints',
-        header: 'Total',
-        cell: (props) => (
-          <span className="font-bold text-indigo-900 dark:text-orange-200">
-            {props.getValue()}
-          </span>
-        ),
-        meta: {
-          align: 'right',
-          classNames: 'pl-0 md:pl-3',
-        },
-      }),
-      columnHelper.display({
-        id: 'expand',
-        header: '',
-        cell: ({ row }) => {
-          return row.getCanExpand() ? (
-            <div className="flex justify-end">
-              <button
-                onClick={row.getToggleExpandedHandler()}
-                className="flex items-center justify-center rounded p-1 transition-colors hover:bg-slate-100 dark:hover:bg-slate-700"
-                aria-label={
-                  row.getIsExpanded() ? 'Collapse details' : 'Expand details'
-                }
-              >
-                <svg
-                  className={`h-4 w-4 transition-transform duration-200 ${
-                    row.getIsExpanded() ? 'rotate-180' : ''
-                  }`}
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M19 9l-7 7-7-7"
-                  />
-                </svg>
-              </button>
-            </div>
-          ) : null;
-        },
-        meta: {
-          align: 'right',
-          classNames: 'w-12',
-        },
-      }),
-    ],
-    [teams, avatars, lastGameWeek]
-  );
+}: OverviewTable2Props) => {
+  const { sortedData, sortOrder, sortProp, handleSort } =
+    useSortedTable<GameWeekResult>(data, 'cumulativeMilaPoints', 'desc');
+  const [expandedTeam, setExpandedTeam] = useState<string | null>(null);
 
   return (
-    <SortableTable
-      data={data}
-      columns={columns}
-      initialSort={[{ id: 'cumulativeMilaPoints', desc: true }]}
-      expandedState={expanded}
-      onExpandedChange={setExpanded}
-      getRowCanExpand={() => true}
-      renderSubComponent={renderSubComponent}
-    />
+    <div className="text-sm">
+      <div className="flex gap-3 pb-2 pl-2 font-bold">
+        <div className="w-4">#</div>
+        <div className="flex-grow">Team</div>
+        <div className="flex gap-1">
+          GW
+          <SortToggle
+            sortDirection={sortProp === 'gwScore' ? sortOrder : undefined}
+            onClick={() => handleSort('gwScore')}
+          />
+        </div>
+        <div className="flex gap-1">
+          Total
+          <SortToggle
+            sortDirection={
+              sortProp === 'cumulativeMilaPoints' ? sortOrder : undefined
+            }
+            onClick={() => handleSort('cumulativeMilaPoints')}
+          />
+        </div>
+        <div className="w-4" />
+      </div>
+      {sortedData.map((result) => {
+        const optimizedImage = avatars.find((a) =>
+          a.src.includes(
+            result.teamName.replace('$', 's').toLowerCase().replaceAll(' ', '_')
+          )
+        )!;
+
+        const deltaPosition =
+          result.milaRankLastWeek === null ||
+          result.milaRankLastWeek === undefined
+            ? 0
+            : result.milaRankLastWeek - result.milaRank;
+
+        const isExpanded = expandedTeam === result.teamName;
+
+        return (
+          <div className="flex flex-col" key={result.teamName}>
+            <div
+              className={`flex gap-2 overflow-hidden p-[0.4rem] ${!isExpanded ? 'border-b border-slate-200 dark:border-slate-700' : ''}`}
+            >
+              <div className="w-4">
+                <PositionDelta pos={result.milaRank} delta={deltaPosition} />
+              </div>
+
+              <div
+                className={`flex h-12 flex-grow items-center rounded-l-full sm:h-12 rank-${result.milaRank}`}
+              >
+                <div>
+                  <picture className="h-12 w-12 rounded-full sm:h-12 sm:w-12">
+                    <source
+                      srcSet={optimizedImage.avif.join(', ')}
+                      sizes={optimizedImage.sizes}
+                      type="image/avif"
+                    />
+                    <source
+                      srcSet={optimizedImage.webp.join(', ')}
+                      sizes={optimizedImage.sizes}
+                      type="image/webp"
+                    />
+                    <img
+                      src={optimizedImage.src}
+                      className="rounded-full"
+                      alt={`${result.teamName} avatar`}
+                    />
+                  </picture>
+                </div>
+                <div className="flex-grow p-2">
+                  <div className={result.milaRank < 4 ? 'font-bold' : ''}>
+                    {result.teamName}
+                  </div>
+                  <div className="flex justify-end gap-4">
+                    <div>{result.gwScore}</div>
+                    <div className="font-bold text-indigo-900 dark:text-orange-200">
+                      {result.cumulativeMilaPoints}
+                    </div>
+                  </div>
+                </div>
+                <div className="item-center flex w-4">
+                  <button
+                    onClick={() =>
+                      setExpandedTeam(isExpanded ? null : result.teamName)
+                    }
+                    className="flex items-center justify-center rounded p-1 transition-colors hover:bg-slate-100 dark:hover:bg-slate-700"
+                    aria-label={'Expand details'}
+                  >
+                    <div className={isExpanded ? 'rotate-180' : ''}>
+                      <Chevron />
+                    </div>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div
+              className={`${isExpanded ? 'max-h-[500px]' : 'max-h-0'} overflow-hidden border-b
+      border-slate-200 opacity-100 transition-all duration-200
+      ease-in-out [will-change:max-height,opacity] dark:border-slate-700`}
+            >
+              <TeamDetailPanel
+                row={result}
+                currentGameWeekResults={currentGameWeekResults}
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
