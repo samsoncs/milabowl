ef355875b9aea4adea115919267e56922a5cc1ac
samsoncsv <samson.svendsen@vipps.no>
2025-08-06 20:33:47 +0200

    updating bombux

--- milabowl-astro/src/pages/_components/Trend.tsx
+++ milabowl-astro/src/pages/_components/Trend.tsx
@@ -102,7 +102,17 @@ const PlayerStandingsChart: React.FC<TrendProps> = ({ teams }) => {
             },
           },
         }}
-        colors={{ scheme: 'category10' }}
+        colors={[
+          '#3B82F6', // blue-500
+          '#F59E0B', // amber-500
+          '#10B981', // emerald-500
+          '#EF4444', // red-500
+          '#8B5CF6', // purple-500
+          '#22C55E', // green-500
+          '#EC4899', // pink-500
+          '#62748e', // violet-500
+          '#F97316', // orange-500
+        ]}
         lineWidth={5}
         activeLineWidth={7}
         inactiveLineWidth={5}
