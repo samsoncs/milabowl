44cdf0ebbf83e244931b58b2903c1cc60fd2dbd1
samsoncsv <samson.svendsen@vipps.no>
2025-08-15 14:06:32 +0200

    updating bombstate to use appsettings

---
+++ milabowl-backend/Milabowl.Processing/Processing/BombState/BombSettings.cs
@@ -0,0 +1,7 @@
+namespace Milabowl.Processing.Processing.BombState;
+
+public class BombSettings
+{
+    public required int InitialBombHolder { get; init; }
+    public required int RandomSeed { get; init; }
+}
