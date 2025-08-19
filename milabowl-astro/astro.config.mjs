7d0b3320fbb9a5eba55e4de1939cc014eaef83dd
samsoncsv <samson.svendsen@vipps.no>
2025-08-19 15:17:41 +0200

    adding blog for logos

--- milabowl-astro/astro.config.mjs
+++ milabowl-astro/astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), mdx()],
   devToolbar: { enabled: false },
   vite: {
     ssr: {
